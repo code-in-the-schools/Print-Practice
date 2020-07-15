a13d47a285598be5071d99cfebe6f6a06a0a2f4d
Naziah Williams <Williamsnaziah14@gmail.com>
2020-07-15 14:39:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Naziah Williams")
+print("My favorite musician is a Kpop artist, from the group called Monsta X. The member named Jooheon is my favorite. My favorite ablum of theirs is the most recent one, Fantasia X. My favorite song from the album is Flow.")
+print("I would love to go on vacation to Greenland.")
+print("A hobby of mines is drawing and I also like to listen to music.")
+print("Todays date is July 14th,2020")
